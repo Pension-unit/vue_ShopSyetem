a8e58ef8fefc3248b9b9c420dbbcd71dbe317e65
fkxiaomao96 <1007558214@qq.com>
2020-08-03 21:47:58 +0800

    派大星的第一天后台管理

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 // 配置请求的跟路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   // console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
